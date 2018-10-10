e986c7bcbcbf3c7e2a627d8d749cf7ed0e25f7f3
Ryan <werner.ryan18@gmail.com>
2018-10-10 13:35:18 -0700

    Add project files.

---
+++ ConsoleApp15/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    public class Class1
+    {
+        public static int Math(int x, int y = 2)
+        {
+
+            x = x + y;
+
+            return x;
+        }
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 holder = new Class1();
+            int num1;
+            int num2;
+            string temp;
+
+            Console.WriteLine("Enter an integer.");
+            temp = Console.ReadLine();
+            num1 = Convert.ToInt32(temp);
+
+            Console.WriteLine("Enter a second integer, or leave it blank.");
+            temp = Console.ReadLine();
+            if (temp == "")
+            {
+                num1 = Class1.Math(num1);
+            }
+            else
+            {
+                num2 = Convert.ToInt32(temp);
+
+                num1 = Class1.Math(num1, num2);
+            }
+
+            
+            Console.WriteLine(num1);
+
+            Console.ReadLine();
+
+        }
+    }
+}
